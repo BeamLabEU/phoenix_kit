bfa92d1e3d73528483b4e45fea1bd20fd96dbbca
Alexander Don <alexdon20022@gmail.com>
2025-12-16 19:48:31 +0200

    Updated beamlab_countries to 1.0.3

--- mix.exs
+++ mix.exs
@@ -133,7 +133,7 @@ defmodule PhoenixKit.MixProject do
       {:igniter, "~> 0.7"},
 
       # Language and country data
-      {:beamlab_countries, "~> 1.0.1"}
+      {:beamlab_countries, "~> 1.0.3"}
     ]
   end
 
