bf8db61376f985da9703d250148553e93b592bbd
timujeen <timujeen@gmail.com>
2025-12-26 12:37:51 +0000

    Add UUIDv7 generation to AI schemas

--- lib/phoenix_kit/ai/endpoint.ex
+++ lib/phoenix_kit/ai/endpoint.ex
@@ -174,6 +174,14 @@ defmodule PhoenixKit.AI.Endpoint do
     |> validate_temperature()
     |> validate_penalties()
     |> maybe_set_default_base_url()
+    |> maybe_generate_uuid()
+  end
+
+  defp maybe_generate_uuid(changeset) do
+    case get_field(changeset, :uuid) do
+      nil -> put_change(changeset, :uuid, UUIDv7.generate())
+      _ -> changeset
+    end
   end
 
   @doc """
--- lib/phoenix_kit/ai/prompt.ex
+++ lib/phoenix_kit/ai/prompt.ex
@@ -136,6 +136,14 @@ defmodule PhoenixKit.AI.Prompt do
     |> unique_constraint(:slug, name: :phoenix_kit_ai_prompts_slug_uidx)
     |> maybe_generate_slug()
     |> auto_extract_variables()
+    |> maybe_generate_uuid()
+  end
+
+  defp maybe_generate_uuid(changeset) do
+    case get_field(changeset, :uuid) do
+      nil -> put_change(changeset, :uuid, UUIDv7.generate())
+      _ -> changeset
+    end
   end
 
   @doc """
