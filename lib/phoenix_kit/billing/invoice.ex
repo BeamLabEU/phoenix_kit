81b1a743ee9f278d86280d70a9584de05888f817
timujeen <timujeen@gmail.com>
2025-12-26 12:42:02 +0000

    Add UUIDv7 generation to billing schemas

--- lib/phoenix_kit/billing/invoice.ex
+++ lib/phoenix_kit/billing/invoice.ex
@@ -143,6 +143,14 @@ defmodule PhoenixKit.Billing.Invoice do
     |> unique_constraint(:invoice_number)
     |> foreign_key_constraint(:user_id)
     |> foreign_key_constraint(:order_id)
+    |> maybe_generate_uuid()
+  end
+
+  defp maybe_generate_uuid(changeset) do
+    case get_field(changeset, :uuid) do
+      nil -> put_change(changeset, :uuid, UUIDv7.generate())
+      _ -> changeset
+    end
   end
 
   @doc """
