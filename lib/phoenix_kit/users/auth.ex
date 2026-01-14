5f6998c0e3839eab5ac311e97b964281f4dac460
Alexander Don <alexdon20022@gmail.com>
2026-01-14 19:37:04 +0200

    add get_by_by_username

--- lib/phoenix_kit/users/auth.ex
+++ lib/phoenix_kit/users/auth.ex
@@ -92,6 +92,22 @@ defmodule PhoenixKit.Users.Auth do
   end
 
   @doc """
+  Gets a user by username.
+
+  ## Examples
+
+      iex> get_user_by_username("johndoe")
+      %User{}
+
+      iex> get_user_by_username("unknown")
+      nil
+
+  """
+  def get_user_by_username(username) when is_binary(username) do
+    Repo.get_by(User, username: username)
+  end
+
+  @doc """
   Gets a user by email and password.
 
   This function includes rate limiting protection to prevent brute-force attacks.
