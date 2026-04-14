b0af5be98202147e6bbdc6e49b1620c1bc8cb10d
mithereal <mithereal@gmail.com>
2026-04-13 20:36:29 -0700

    fix: blockers addressed migration removed sh script removed guide

--- lib/phoenix_kit/migrations/postgres/v97.ex
+++ lib/phoenix_kit/migrations/postgres/v97.ex
@@ -14,7 +14,7 @@ defmodule PhoenixKit.Migrations.Postgres.V97 do
                            primary_key: false,
                            prefix: prefix
                          ) do
-      add(:widget_uuid, :uuid, primary_key: true, default: fragment("uuid_generate_v7()"))
+      add(:uuid, :uuid, primary_key: true, default: fragment("uuid_generate_v7()"))
       add(:x, :integer, null: false)
       add(:y, :integer, null: false)
       add(:w, :integer, null: false)
@@ -32,23 +32,17 @@ defmodule PhoenixKit.Migrations.Postgres.V97 do
       )
     end
 
-    execute("COMMENT ON TABLE #{p}phoenix_kit IS '97'")
+    execute("COMMENT ON TABLE #{p}phoenix_kit_dashboard_layouts IS '97'")
   end
 
   def down(opts) do
     prefix = Map.get(opts, :prefix, "public")
     p = prefix_str(prefix)
 
-    drop_if_exists(index(:phoenix_kit_files, [:folder_uuid], prefix: prefix))
+    drop_if_exists(index(:phoenix_kit_dashboard_layouts, [], prefix: prefix))
 
-    alter table(:phoenix_kit_files, prefix: prefix) do
-      remove_if_exists(:folder_uuid, :uuid)
-    end
 
-    drop_if_exists(table(:phoenix_kit_media_folder_links, prefix: prefix))
-    drop_if_exists(table(:phoenix_kit_media_folders, prefix: prefix))
-
-    execute("COMMENT ON TABLE #{p}phoenix_kit IS '94'")
+    execute("COMMENT ON TABLE #{p}phoenix_kit_dashboard_layouts IS '97'")
   end
 
   defp prefix_str("public"), do: ""
